04945951956d7a29fe79dd48d9cdce73c0d70710
Michael Kennedy <michaelkennedyns5@gmail.com>
2022-08-23 18:00:44 -0700

    Updating readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const greetings = {
+  morning: "Good morning",
+  noon: "Good afternoon",
+  evening: "Good evening",
+  night: "Good night",
+};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      greeting: greetings.morning,
+    };
+    this.startTime = this.startTime.bind(this);
+  }
+
+  componentDidMount() {
+    this.startTime();
+  }
+
+  startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let greeting;
+    if (h > 6 && h < 12) {
+      greeting = greetings.morning;
+    } else if (h >= 12 && h < 17) {
+      greeting = greetings.noon;
+    } else if (h >= 17 && h < 20) {
+      greeting = greetings.evening;
+    } else {
+      greeting = greetings.night;
+    }
+    this.setState({ greeting });
+    setTimeout(this.startTime, 1000);
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <h1>{this.state.greeting}</h1>
+        </div>
+    );
+  }
 }
 
 export default App;
