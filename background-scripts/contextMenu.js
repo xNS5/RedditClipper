534b2c10e05aa52b16005e4060af6b0f8fc27894
Michael Kennedy <michaelkennedyns5@gmail.com>
2022-10-29 15:52:46 -0700

    Trying to add a text replacement feature, not working yet lol

--- background-scripts/contextMenu.js
+++ background-scripts/contextMenu.js
@@ -4,20 +4,38 @@ browser.contextMenus.create({
     contexts: ["all"]
 });
 
+browser.contextMenus.create({
+    id: "create-link",
+    title: "Link",
+    contexts: ["all"]
+})
+
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    var text = "";
-    var curr_tab = {...tab};
-    var curr_info = {...info};
-    if(curr_info.selectionText){
-        var filtered_arr = curr_info.selectionText.replace(/((\r?\n|\r|\t)\d*)+(\r?\n|\r)/gm, "\n").trim().split("\n");
-        filtered_arr.forEach((string) => {
-            text += (`> ${string}\n\n\n`);
+    if(info.menuItemId === "copy-text"){
+        let text = "";
+        if(info.selectionText){
+            let filtered_arr = info.selectionText.replace(/((\r?\n|\r|\t)\d*)+(\r?\n|\r)/gm, "\n").trim().split("\n");
+            filtered_arr.forEach((string) => {
+                text += (`> ${string}\n\n`);
+            })
+            console.log(info);
+        }
+
+        text += `[${text.length > 0 ? "Source" : tab.title}](${tab.url})`;
+
+        navigator.clipboard.writeText(text).then(() => {
+            console.log(`Added "${text}" to clipboard`)
         })
+    } else if(info.menuItemId === "create-link"){
+        navigator.clipboard.readText().then((clipText) => {
+            if(info.editable){
+            const re = new RegExp('^(https?:\/\/)')
+            const selectedText = info.selectionText ? info.selectionText : "";
+            const clipboardUrl = re.test(clipText) ? clipText : "";
+            console.log(`[${selectedText}](${clipboardUrl})`);
+            }
+        });
+
     }
 
-    text += `[${text.length > 0 ? "Source" : curr_tab.title}](${curr_tab.url})`;
-
-    navigator.clipboard.writeText(text).then(() => {
-        console.log(`Added "${text}" to clipboard`)
-    })
 });
